c76567e3fc57ede75d253b98e983a54b36e4a47b
Tim Ermilov <yamalight@gmail.com>
2016-09-16 11:28:23 +0200

    Disable chalk to get correct output during coverage

--- test/main.js
+++ test/main.js
@@ -1,13 +1,17 @@
 // npm packages
 import test from 'tape';
+import chalk from 'chalk';
 
 // our packages
 import {startServer, stopServer} from './fixtures/server';
-// import cli from '../src/index';
+// import '../src/index';
 
 // tests
 import loginTest from './login';
 import endpointTest from './endpoint';
+
+// disable chalk
+chalk.enabled = false;
 
 // start server before running tests
 test('Start server', (t) => {
