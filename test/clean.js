7033f4b66097da8b33edbb4ffb31ab224c9441c5
Tim Ermilov <yamalight@gmail.com>
2016-09-16 13:43:00 +0200

    Add basic deauth handling test

--- test/clean.js
+++ test/clean.js
@@ -6,9 +6,15 @@ import app from './fixtures/server';
 import clean from '../src/clean';
 
 export default (test) => {
-  // create clean method
+  // create clean method that fails on second call
+  let calls = 0;
   app.post('/api/clean', (req, res) => {
-    res.sendStatus(204);
+    if (calls === 0) {
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(403);
+    }
+    calls += 1;
   });
 
   // test
@@ -23,6 +29,25 @@ export default (test) => {
         ['Cleaning docker on:', 'http://localhost:3000'],
         [],
         ['Docker cleaned!'],
+      ], 'Correct log output');
+      // restore console
+      console.log.restore();
+      t.end();
+    });
+  });
+
+  // test
+  test('Should catch error during clean', (t) => {
+    // spy on console
+    const consoleSpy = sinon.spy(console, 'log');
+    // execute login
+    clean.handler().then(() => {
+      // make sure log in was successful
+      // first check console output
+      t.deepEqual(consoleSpy.args, [
+        ['Cleaning docker on:', 'http://localhost:3000'],
+        [],
+        ['Authentication token expired!', 'Please re-login'],
       ], 'Correct log output');
       // restore console
       console.log.restore();
