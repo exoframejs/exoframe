9958a0093865df086ee0a6a133cf302faf83094c
Tim Ermilov <yamalight@gmail.com>
2017-05-18 14:10:56 +0200

    Add test for failed login

--- test/login.js
+++ test/login.js
@@ -14,16 +14,17 @@ module.exports = () => {
   const token = 'test-token';
   const user = {username: 'admin', password: 'admin'};
   const wrongUser = {username: 'wrong', password: 'user'};
+  const failedUser = {username: 'failed', password: 'user'};
 
   // handle correct request
   const correctLogin = nock('http://localhost:8080').post('/login', user).reply(200, {
     token,
     user,
   });
-
   const brokenLogin = nock('http://localhost:8080').post('/login', wrongUser).reply(401);
+  const failedLogin = nock('http://localhost:8080').post('/login', failedUser).reply(200, {});
 
-  // test
+  // test login
   tap.test('Should login', t => {
     // stup inquirer answers
     sinon.stub(inquirer, 'prompt').callsFake(() => Promise.resolve(user));
@@ -53,7 +54,7 @@ module.exports = () => {
     });
   });
 
-  // test
+  // test wrong credentials
   tap.test('Should fail to login with wrong credentials', t => {
     // stup inquirer answers
     sinon.stub(inquirer, 'prompt').callsFake(() => Promise.resolve(wrongUser));
@@ -78,6 +79,34 @@ module.exports = () => {
       const cfg = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
       t.equal(cfg.token, token, 'Correct token');
       t.equal(cfg.user.username, user.username, 'Correct username');
+      // restore inquirer
+      inquirer.prompt.restore();
+      // restore console
+      console.log.restore();
+      t.end();
+    });
+  });
+
+  // test failure
+  tap.test('Should handle failure', t => {
+    // stup inquirer answers
+    sinon.stub(inquirer, 'prompt').callsFake(() => Promise.resolve(failedUser));
+    // spy on console
+    const consoleSpy = sinon.spy(console, 'log');
+    // execute login
+    login().then(() => {
+      // make sure log in was successful
+      // check that server was called
+      t.ok(failedLogin.isDone());
+      // first check console output
+      t.deepEqual(
+        consoleSpy.args,
+        [
+          ['Logging in to:', 'http://localhost:8080'],
+          ['Error logging in!', 'Check your username and password and try again.'],
+        ],
+        'Correct log output'
+      );
       // restore inquirer
       inquirer.prompt.restore();
       // restore console
