a999f42b7c370a59bfe8b976f594c5bb3cf7ee73
Tim Ermilov <yamalight@gmail.com>
2017-05-18 14:07:57 +0200

    Add tests for deauth on deploy and remove

--- test/deploy.js
+++ test/deploy.js
@@ -7,6 +7,7 @@ const sinon = require('sinon');
 
 // our packages
 const {handler: deploy} = require('../src/commands/deploy');
+const {userConfig, updateConfig} = require('../src/config');
 
 module.exports = () => {
   const folder = 'test_html_project';
@@ -44,6 +45,43 @@ module.exports = () => {
       );
       // restore console
       console.log.restore();
+      // tear down nock
+      deployServer.done();
+      t.end();
+    });
+  });
+
+  // test
+  tap.test('Should deauth on 401', t => {
+    // copy original config for restoration
+    const originalConfig = Object.assign({}, userConfig);
+    // handle correct request
+    const deployServer = nock('http://localhost:8080').post('/deploy').reply(401, {error: 'Deauth test'});
+    // spy on console
+    const consoleSpy = sinon.spy(console, 'log');
+    // execute login
+    deploy({_: [folderPath]}).then(() => {
+      // make sure log in was successful
+      // check that server was called
+      t.ok(deployServer.isDone());
+      // first check console output
+      t.deepEqual(
+        consoleSpy.args,
+        [
+          [`Deploying ${folderPath} to endpoint:`, 'http://localhost:8080'],
+          ['Error: authorization expired!', 'Please, relogin and try again.'],
+        ],
+        'Correct log output'
+      );
+      // check config
+      t.notOk(userConfig.user, 'Should not have user');
+      t.notOk(userConfig.token, 'Should not have token');
+      // restore console
+      console.log.restore();
+      // tear down nock
+      deployServer.done();
+      // restore original config
+      updateConfig(originalConfig);
       t.end();
     });
   });
--- test/remove.js
+++ test/remove.js
@@ -5,15 +5,15 @@ const sinon = require('sinon');
 
 // our packages
 const {handler: remove} = require('../src/commands/remove');
+const {userConfig, updateConfig} = require('../src/config');
 
 module.exports = () => {
   const id = 'test-id';
 
-  // handle correct request
-  const rmServer = nock('http://localhost:8080').post(`/remove/${id}`).reply(204);
-
-  // test
+  // test removal
   tap.test('Should remove', t => {
+    // handle correct request
+    const rmServer = nock('http://localhost:8080').post(`/remove/${id}`).reply(204);
     // spy on console
     const consoleSpy = sinon.spy(console, 'log');
     // execute login
@@ -25,6 +25,39 @@ module.exports = () => {
       t.deepEqual(consoleSpy.args, [['Removing deployment:', id], ['Deployment removed!']], 'Correct log output');
       // restore console
       console.log.restore();
+      rmServer.done();
+      t.end();
+    });
+  });
+
+  // test
+  tap.test('Should deauth on 401', t => {
+    // copy original config for restoration
+    const originalConfig = Object.assign({}, userConfig);
+    // handle correct request
+    const rmServer = nock('http://localhost:8080').post(`/remove/${id}`).reply(401);
+    // spy on console
+    const consoleSpy = sinon.spy(console, 'log');
+    // execute login
+    remove({id}).then(() => {
+      // make sure log in was successful
+      // check that server was called
+      t.ok(rmServer.isDone());
+      // first check console output
+      t.deepEqual(
+        consoleSpy.args,
+        [['Removing deployment:', id], ['Error: authorization expired!', 'Please, relogin and try again.']],
+        'Correct log output'
+      );
+      // check config
+      t.notOk(userConfig.user, 'Should not have user');
+      t.notOk(userConfig.token, 'Should not have token');
+      // restore console
+      console.log.restore();
+      // tear down nock
+      rmServer.done();
+      // restore original config
+      updateConfig(originalConfig);
       t.end();
     });
   });
