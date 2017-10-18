61616fd0961779196e7c0855fe2457b565d825e4
Tim Ermilov <yamalight@gmail.com>
2017-10-18 13:28:11 +0200

    Ask if user wants to execute update immediately when available, closes #68

--- test/update.js
+++ test/update.js
@@ -2,6 +2,7 @@
 const tap = require('tap');
 const nock = require('nock');
 const sinon = require('sinon');
+const inquirer = require('inquirer');
 
 // our packages
 const {handler: update} = require('../src/commands/update');
@@ -108,6 +109,8 @@ module.exports = () => {
       .reply(200, response);
     // spy on console
     const consoleSpy = sinon.spy(console, 'log');
+    // stup inquirer answers
+    sinon.stub(inquirer, 'prompt').callsFake(() => Promise.resolve({upServer: false, upTraefik: false}));
     // execute login
     update({}).then(() => {
       // make sure log in was successful
@@ -125,12 +128,79 @@ module.exports = () => {
           ['Traefik:'],
           ['  current: v1.3.0'],
           ['  latest: v1.3.2'],
+          [],
         ],
         'Correct log output'
       );
       // restore console
       console.log.restore();
+      // restore inquirer
+      inquirer.prompt.restore();
+      // cleanup server
       updateServer.done();
+      t.end();
+    });
+  });
+
+  // test version check
+  tap.test('Should update all on user prompt', t => {
+    // handle correct request
+    const response = {
+      server: '0.18.0',
+      latestServer: '0.19.1',
+      serverUpdate: true,
+      traefik: 'v1.3.0',
+      latestTraefik: 'v1.3.2',
+      traefikUpdate: true,
+    };
+    const updateInfoServer = nock('http://localhost:8080')
+      .get('/version')
+      .reply(200, response);
+    const updateServerRun = nock('http://localhost:8080')
+      .post('/update/server')
+      .reply(200, {updated: true});
+    const updateTraefikRun = nock('http://localhost:8080')
+      .post('/update/traefik')
+      .reply(200, {updated: true});
+    // spy on console
+    const consoleSpy = sinon.spy(console, 'log');
+    // stup inquirer answers
+    sinon.stub(inquirer, 'prompt').callsFake(() => Promise.resolve({upServer: true, upTraefik: true}));
+    // execute login
+    update({}).then(() => {
+      // make sure log in was successful
+      // check that servers were called
+      t.ok(updateInfoServer.isDone());
+      t.ok(updateServerRun.isDone());
+      t.ok(updateTraefikRun.isDone());
+      // first check console output
+      t.deepEqual(
+        consoleSpy.args,
+        [
+          [],
+          ['Exoframe Server:'],
+          ['  current: 0.18.0'],
+          ['  latest: 0.19.1'],
+          [],
+          ['Traefik:'],
+          ['  current: v1.3.0'],
+          ['  latest: v1.3.2'],
+          [],
+          ['Updating traefik on:', 'http://localhost:8080'],
+          ['Successfully updated traefik!'],
+          ['Updating server on:', 'http://localhost:8080'],
+          ['Successfully updated server!'],
+        ],
+        'Correct log output'
+      );
+      // restore console
+      console.log.restore();
+      // restore inquirer
+      inquirer.prompt.restore();
+      // cleanup server
+      updateInfoServer.done();
+      updateServerRun.done();
+      updateTraefikRun.done();
       t.end();
     });
   });
