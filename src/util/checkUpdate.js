d573a80e21546ca77ff06aaf871cef37670b868a
Tim Ermilov <yamalight@gmail.com>
2019-11-07 13:36:42 +0100

    Replace update-notifier with smaller packages

--- src/util/checkUpdate.js
+++ src/util/checkUpdate.js
@@ -1,29 +1,27 @@
 // npm packages
 const chalk = require('chalk');
-const updateNotifier = require('update-notifier');
+const latestVersion = require('latest-version');
+const semverDiff = require('semver-diff');
 
 // packaged script path
 const pkgPath = '/snapshot/exoframe-cli/src/util';
 
 // check function
-module.exports = pkg => {
+module.exports = async pkg => {
+  const current = '5.0.0'; // pkg.version;
   // Checks for available update and returns an instance
-  const notifier = updateNotifier({
-    pkg,
-    updateCheckInterval: 1000,
-  });
+  const latest = await latestVersion('exoframe').then(r => r.trim());
   // show message if update is available
-  if (notifier.update) {
-    const {update} = notifier;
+  if (semverDiff(current, latest)) {
     const isPackaged = __dirname === pkgPath;
     const upNpmMsg = `Run ${chalk.cyan('npm i -g exoframe')} to update`;
     const upPkgMsg = `Download from ${chalk.cyan('https://github.com/exoframejs/exoframe/releases')}`;
     const upmsg = isPackaged ? upPkgMsg : upNpmMsg;
-    const message = `Update available ${chalk.dim(update.current)} ${chalk.reset('→')} ${chalk.green(update.latest)}`;
+    const message = `Update available ${chalk.dim(current)} ${chalk.reset('→')} ${chalk.green(latest)}`;
     console.log(`
   ┌───────────────────────────────────────┐
   │                                       │
-  │        ${message}       │
+  │     ${message}    │
   │    ${upmsg}    │
   │                                       │
   └───────────────────────────────────────┘
