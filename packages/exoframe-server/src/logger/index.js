b47b48925d26c6eb075172ef2628f1342e7aafc1
Tim Ermilov <yamalight@gmail.com>
2021-10-29 12:12:30 +0200

    [exoframe-server] Fix pino-pretty transport usage warning

--- packages/exoframe-server/src/logger/index.js
+++ packages/exoframe-server/src/logger/index.js
@@ -4,10 +4,21 @@ import pino from 'pino';
 const levelTesting = process.env.NODE_ENV === 'testing' ? 'error' : false;
 const level = levelTesting || (process.env.NODE_ENV === 'production' ? 'warn' : 'debug');
 
+// use pino-pretty in debug mode
+const transport =
+  process.env.NODE_ENV === 'production'
+    ? {}
+    : {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+        },
+      };
+
 const logger = pino({
   name: 'exoframe-server',
   level,
-  prettyPrint: process.env.NODE_ENV !== 'production',
+  transport,
 });
 
 export default logger;
