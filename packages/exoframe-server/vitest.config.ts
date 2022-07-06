c2fcafc7d9d2f45468e00e0646e3a6818f97d835
Tim Ermilov <yamalight@gmail.com>
2022-07-06 15:46:23 +0200

    [exoframe-server] Increase hookTimeout for vite tests for slower CI

--- packages/exoframe-server/vitest.config.ts
+++ packages/exoframe-server/vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     // increased timeouts for slower CI runners
     testTimeout: 120000,
+    hookTimeout: 120000,
     // run in single threaded mode to prevent container conflicts during tests
     threads: false,
   },
