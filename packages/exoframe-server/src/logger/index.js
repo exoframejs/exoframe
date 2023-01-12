3f551fcf413695792b5ec7b844cce32f9cfc5a03
Tim Ermilov <yamalight@gmail.com>
2023-01-12 13:02:14 +0100

    [exoframe-server] Fix pino logging transports in production

--- packages/exoframe-server/src/logger/index.js
+++ packages/exoframe-server/src/logger/index.js
@@ -1,4 +1,10 @@
+import { homedir } from 'os';
+import { join } from 'path';
 import pino from 'pino';
+
+// construct paths
+const xdgConfigHome = process.env.XDG_CONFIG_HOME || join(homedir(), '.config');
+const logFolder = join(xdgConfigHome, 'exoframe', 'exoframe-server');
 
 // prepare level
 const levelTesting = process.env.NODE_ENV === 'testing' ? 'error' : false;
@@ -7,7 +13,10 @@ const level = levelTesting || (process.env.NODE_ENV === 'production' ? 'warn' : 
 // use pino-pretty in debug mode
 const transport =
   process.env.NODE_ENV === 'production'
-    ? {}
+    ? pino.transport({
+        target: 'pino/file',
+        options: { destination: join(logFolder, 'log.txt'), append: true },
+      })
     : {
         target: 'pino-pretty',
         options: {
@@ -18,7 +27,7 @@ const transport =
 const logger = pino({
   name: 'exoframe-server',
   level,
-  transport,
+  targets: [transport],
 });
 
 export default logger;
