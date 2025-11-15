0564ad2cc5656048f87fed9e52e9535d24d5a3e9
Tim Ermilov <yamalight@gmail.com>
2025-11-15 16:13:55 +0100

    [exoframe-cli] Include deployment name into list details

--- packages/exoframe-cli/src/util/renderDeployments.js
+++ packages/exoframe-cli/src/util/renderDeployments.js
@@ -4,14 +4,14 @@ import { tableBorder, tableStyle } from '../config/table.js';
 export const renderDeployments = (deployments) => {
   // create table
   const resultTable = new Table({
-    head: ['ID', 'URL', 'Hostname', 'Type'],
+    head: ['ID', 'Deployment Name', 'URL', 'Hostname', 'Type'],
     chars: tableBorder,
     style: tableStyle,
   });
 
   // process deployments
-  deployments.forEach(({ name, domain, host, type }) => {
-    resultTable.push([name, domain, host, type]);
+  deployments.forEach(({ name, deploymentName, domain, host, type }) => {
+    resultTable.push([name, deploymentName, domain, host, type]);
   });
 
   // draw table
