f21885c193e7169d7fa969a1433f841ca8ee4605
Tim Ermilov <yamalight@gmail.com>
2025-12-16 10:24:35 +0100

    Fix formatting in eslint config files

--- packages/exoframe-client/eslint.config.mjs
+++ packages/exoframe-client/eslint.config.mjs
@@ -1,37 +1,39 @@
-import { defineConfig } from "eslint/config";
-import prettier from "eslint-plugin-prettier";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([{
-    extends: compat.extends("eslint:recommended", "prettier"),
+export default defineConfig([
+  {
+    extends: compat.extends('eslint:recommended', 'prettier'),
 
     plugins: {
-        prettier,
+      prettier,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.node,
-        },
+      globals: {
+        ...globals.node,
+      },
     },
 
     rules: {
-        camelcase: "off",
-        "promise/param-names": "off",
-        "prefer-promise-reject-errors": "off",
-        "no-control-regex": "off",
-        "prettier/prettier": "error",
+      camelcase: 'off',
+      'promise/param-names': 'off',
+      'prefer-promise-reject-errors': 'off',
+      'no-control-regex': 'off',
+      'prettier/prettier': 'error',
     },
-}]);
+  },
+]);
