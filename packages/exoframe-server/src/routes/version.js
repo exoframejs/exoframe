cced80286162398a676dfad3ded6c64165c10ddc
Tim Ermilov <yamalight@gmail.com>
2026-02-22 18:21:02 +0100

    Fetch latest server version from correct github repo

--- packages/exoframe-server/src/routes/version.js
+++ packages/exoframe-server/src/routes/version.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 import docker from '../docker/docker.js';
 
 // urls for tags request
-const exoServerUrl = `https://api.github.com/repos/exoframejs/exoframe-server/releases`;
+const exoServerUrl = `https://api.github.com/repos/exoframejs/exoframe/releases`;
 const traefikUrl = 'https://api.github.com/repos/containous/traefik/releases';
 const traefikVersionPrefix = 'v3';
 
